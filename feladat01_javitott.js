c590d0f63ff330eeac4272acdc6afed91e21cee4
sandravarga <sandravarga@bitbucket.org>
2023-01-25 00:11:08 +0100

    javitas az utolso email alapjan

--- feladat01_javitott.js
+++ feladat01_javitott.js
@@ -1,36 +1,37 @@
-function hanySzinKeverheto(szinek) {
-
-    for(let i = 0; i < szinek.length; i++) {
-        if(typeof szinek[i] !== "string") {
-            console.log("Hiba! Az egyik elem nem string.")
-            return 0;
-        }
-    }
-
-    for(let i = 0; i < szinek.length; i++) {
-        if(szinek[i] !== "piros" && szinek[i] !== "zöld" && szinek[i] !== "kék") {
-            console.log("Hiba! Csak a tömbben szereplő alapszínekből keverhető a szín.")
-            return 0;
-        }
-    }
-
-    if(szinek.length == 0 || szinek.length == 1) {
-        return 0;
-    }
-
-    for(let i =0 ; i < szinek.length; i++) {
-        if(szinek[0] === szinek[1] || szinek[0] === szinek[2] || szinek[1] === szinek[2]) {
-            console.log("Hiba! Csak különböző alapszínekből keverhető a szín.");
-            return 0;
-        } 
-    }
-
-    if(szinek.length == 2) {
+function faktorialis(szam) {
+    let eredmeny = 1;
+    if(szam == 0) {
         return 1;
-    } else if(szinek.length == 3) {
-        return 4;
-    } else {
-        console.log("Hiba! Túl sok színt adtál meg!");
+    }
+    for(let i=1; i<szam+1; i++) {
+        eredmeny = eredmeny*i;
+    }
+    return eredmeny;
+}
+
+function stringMindenElem(szinek) {
+    let eredmeny = true;
+    for(let i=0; i<szinek.length; i++) {
+        if(typeof szinek[i] !== "string") {
+            eredmeny = false;
+            break;
+        }
+    }
+    return eredmeny;
+}
+
+
+function szinkeveres(szinek) {
+    if(!stringMindenElem(szinek)) {
+        console.log("Csak string lehet");
         return 0;
     }
+
+    let lehetosegekSzama = 0;
+    for(let i=2; i<szinek.length+1; i++) {
+       lehetosegekSzama = lehetosegekSzama + (faktorialis(szinek.length) / (faktorialis(i) * faktorialis(szinek.length - i)));
+    }
+    return lehetosegekSzama;
 }
+
+//console.log(szinkeveres(["sárga","kék","zöld","piros","szürke"])); = 26
